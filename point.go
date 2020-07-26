00bcb680a8d89b52d891b9e66968de3b4b1dde56
Urogna <q.mauro@gmail.com>
2020-07-26 20:16:47 +0200

    added comments

--- point.go
+++ point.go
@@ -1,42 +1,47 @@
 package point
 
 import (
-	_ "fmt"
 	"image"
 	"strconv"
 )
 
-// MovingPoint is a Point that has a Point and accelleration assigned to it.
+// MovingPoint is a Point that has position, speed and accelleration assigned to it.
 type MovingPoint struct {
 	p Point
 	s Point
 	a Point
 }
 
+// next() updates the position and speed of the MovingPoint based on its speed and accelleration.
 func (m *MovingPoint) next() {
 	m.p.Add(&m.s)
 	m.s.Add(&m.a)
 }
 
+// Field is a rectangle with the center position.
 type Field struct {
 	image.Rectangle
 	c Point
 }
 
+// Fld initializes a Field.
 func Fld(r image.Rectangle) Field {
 	c := Point{float64(r.Min.X+r.Max.X) / 2, float64(r.Min.Y+r.Max.Y) / 2}
 	return Field{r, c}
 }
 
+// Point is a 2d vector, with float values as its coordinates.
 type Point struct {
 	x, y float64
 }
 
-func (p1 *Point) Add(p2 *Point) {
-	p1.x += p2.x
-	p1.y += p2.y
+// Add adds 2 Points.
+func (p *Point) Add(q *Point) {
+	p.x += q.x
+	p.y += q.y
 }
 
+// String converts a point into a string.
 func (p *Point) String() string {
 	return "(" + strconv.FormatFloat(p.x, 'e', 2, 64) + " , " + strconv.FormatFloat(p.y, 'e', 2, 64) + ")"
 }
